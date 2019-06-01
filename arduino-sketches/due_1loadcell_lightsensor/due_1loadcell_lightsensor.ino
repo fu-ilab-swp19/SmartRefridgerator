3500ddba8c3a13f335a294a6171e5bf6273f9928
krasoa <krasoa@github.com>
2019-06-01 14:44:21 +0200

    Prototyp - Sendet Gewicht an Server, misst Gewicht nur wenn Licht an ist + Kalibrierungsprogramm fuer Gewichtszellen

--- arduino-sketches/due_1loadcell_lightsensor/due_1loadcell_lightsensor.ino
+++ arduino-sketches/due_1loadcell_lightsensor/due_1loadcell_lightsensor.ino
@@ -13,8 +13,7 @@
 #include <BH1750.h>
 
 //HX711 constructor (dout pin, sck pin):
-HX711_ADC LoadCellRO(2,3); // red orange power
-HX711_ADC LoadCellBW(5,6);  // black white power
+HX711_ADC LoadCellRO(4,5); // red orange power
 
 //BH1750 LightMeter Contructor
 // 5V = VCC
@@ -23,38 +22,26 @@ HX711_ADC LoadCellBW(5,6);  // black white power
 // A5 = SDL
 BH1750 lightMeter;
 
-const int eepromAdress = 0;
-
 long t;
 
 void setup() {
   
   float calValue; // calibration value
   calValue = 696.0; // uncomment this if you want to set this value in the sketch 
-  #if defined(ESP8266) 
-  //EEPROM.begin(512); // uncomment this if you use ESP8266 and want to fetch the value from eeprom
-  #endif
-  //EEPROM.get(eepromAdress, calValue); // uncomment this if you want to fetch the value from eeprom
   
   Serial.begin(9600); delay(10);
   Wire.begin();
   Serial.println();
   Serial.println("Starting...");
   LoadCellRO.begin();
-  LoadCellBW.begin();
   lightMeter.begin();
   long stabilisingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilising time
   LoadCellRO.start(stabilisingtime);
-  LoadCellBW.start(stabilisingtime);
   if(LoadCellRO.getTareTimeoutFlag()) {
     Serial.println("Tare timeout, check MCU>HX711 wiring and pin designations -- RO");
   }
-  else if(LoadCellBW.getTareTimeoutFlag()) {
-    Serial.println("Tare timeout, check MCU>HX711 wiring and pin designations -- BW");
-  }
   else {
     LoadCellRO.setCalFactor(calValue); // set calibration value (float)
-    LoadCellBW.setCalFactor(calValue);
     Serial.println("Startup + tare is complete");
   }
 }
@@ -63,16 +50,12 @@ void loop() {
   //update() should be called at least as often as HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS
   //use of delay in sketch will reduce effective sample rate (be carefull with use of delay() in the loop)
   LoadCellRO.update();
-  LoadCellBW.update();
   float lux = lightMeter.readLightLevel();
 
   //get smoothed value from data set
   if (lux > 5){
     float i = LoadCellRO.getData();
     Serial.print("Load_cell RO output val: ");
-    Serial.print(i);
-    i = LoadCellBW.getData();
-    Serial.print("\t\tLoad_cell BW output val: ");
     Serial.print(i);
     Serial.print("\tLux: ");
     Serial.println(lux);
@@ -84,12 +67,11 @@ void loop() {
     char inByte = Serial.read();
     if (inByte == 't') {
       LoadCellRO.tareNoDelay();
-      LoadCellBW.tareNoDelay();
     }
   }
 
   //check if last tare operation is complete
-  if (LoadCellRO.getTareStatus() == true && LoadCellBW.getTareStatus() == true) {
+  if (LoadCellRO.getTareStatus() == true) {
     Serial.println("Tare complete");
   }
 
